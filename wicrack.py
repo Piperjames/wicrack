bc7cb8ba1582ccc53886650a10a495e59872bea6
jamesstefens18 <fireisaverywickedmaster@gmail.com>
2017-12-13 13:04:48 +0000

    still on menu

--- wicrack.py
+++ wicrack.py
@@ -3,37 +3,38 @@
 import os
 from threading import Thread
 import time
+from subprocess import check_output
 
 
 class Wicrack(object):
     """Main class of the program"""
     __author__ = 'piper'
+    __version__ = 0.1
 
     def info():
-        print(" Wicrack v0.1")
-        print(" By %s"%Wicrack.__author__)
-        print(" github: https://github.com/piperjames/wicrack")
+        print(" Wicrack v{}".format(Wicrack.__version__))
+        print(" By {}".format(Wicrack.__author__))
+        print(" git: https://github.com/piperjames/wicrack.git")
 
     menu_options = {-1: {-1: 'Select Job',
-                        1: {'Wifi': {-1: 'Select Interface> ',
-                                     1: ['wlan0'],
+                        1: {'Wifi': {1: ['self.interface()'],
                                      0: 'Back<<'}},
                         2: {'WebApp':   {-1: 'Select Attack Method',
-                                     1: ['SQL Injection (SQLi)'],
-                                     2: ['Cross Site Scripting (Xss)'],
-                                     3: ['Cross Site Request Forgery (CRSF)'],
-                                     4: ['Content Spoofing'],
-                                     5: ['Clickjacking'],
-                                     6: ['Tabnabbing'],
-                                     7: ['Denial of Service'],
-                                     8: ['Man-In-The-Browser'],
-                                     9: ['Phishing'],
+                                     1: 'SQL Injection (SQLi)',
+                                     2: 'Cross Site Scripting (Xss)',
+                                     3: 'Cross Site Request Forgery (CRSF)',
+                                     4: 'Content Spoofing',
+                                     5: 'Clickjacking',
+                                     6: 'Tabnabbing',
+                                     7: 'Denial of Service',
+                                     8: 'Man-In-The-Browser',
+                                     9: 'Phishing',
                                      0: 'Back<<'}
                             },
                         3: {'Network':  {-1: 'Select Attack Method',
-                                     1: ['Bruteforcing'],
-                                     2: ['Man-In-The-Middle'],
-                                     3: ['Buffer Overflow'],
+                                     1: 'Bruteforcing',
+                                     2: 'Man-In-The-Middle',
+                                     3: 'Buffer Overflow',
                                      0: 'Back<<'}}
                         }
                     }
@@ -48,22 +49,24 @@ class Wicrack(object):
         print("")
         for key in self.current_menu.keys():
             if key == -1:
-                print(self.current_menu[key])
+                print(".:%s:."%self.current_menu[key])
             elif str(key).isalpha():
                 for opt in self.current_menu[key].keys():
                     if opt == -1:
-                        print(" [%s] %s"%(key, self.current_menu[key][opt]))
+                        print(".:%s:."%self.current_menu[key][opt])
                     elif opt == 0:
                         print(" [%d] %s"%(opt, self.current_menu[key][opt]))
                     else:
-                        print(" [%d] %s"%(opt, self.current_menu[key][opt][0]))
+                        if len(self.current_menu[key][opt]) == 1:
+                            self.current_menu[key][opt] = eval(self.current_menu[key][opt][0])
+                        print(" [%d] %s"%(opt, self.current_menu[key][opt]))
             else:
                 for opt in self.current_menu[key].keys():
                     print(" [%d] %s"%(key, opt))
         print("")
         while True:
             try:
-                job = int(input("Choose Option> "))
+                job = int(input("-|Choose Option| "))
                 if job == 0:
                     if len(self.current_menu_option) > 1:
                         self.current_menu_option.pop()
@@ -77,16 +80,21 @@ class Wicrack(object):
                     self.current_menu = self.current_menu[job]
                     self.menu()
             except KeyboardInterrupt:
-                time.sleep(1)
+                #time.sleep(1)
                 print('\nGoodbye :)')
-                time.sleep(1)
+                #time.sleep(1)
                 print('Exiting...')
-                time.sleep(1)
+                #time.sleep(1)
                 exit(1)
             except Exception as e:
                 print(str(e))
                 continue
 
+    def interface(self, iface='wlan0'):
+        inface = input('Enter Interface to use [wlan0]: ')
+        if not inface:
+            inface = iface
+        return inface
 
 
 def capture_handshake(bssid, channel, interface='wlan0'):
